acd1adff929516d7da141aef68631a7ee6941b2c
Naomi Seyfer <naomi@seyfer.org>
2018-09-25 12:08:20 -0700

    Make example use the PORT env var instead of 8080 (#415)

--- serving/samples/helloworld-python/app.py
+++ serving/samples/helloworld-python/app.py
@@ -10,4 +10,4 @@ def hello_world():
     return 'Hello World: {}!\n'.format(target)
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=8080)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
