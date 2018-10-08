814de16e7b80021e5347235454e7b64c018974a1
Joseph Burnett <josephburnett79@gmail.com>
2018-10-08 17:11:56 -0400

    Build uberjar for docker image.

--- serving/samples/helloworld-clojure/src/helloworld/core.clj
+++ serving/samples/helloworld-clojure/src/helloworld/core.clj
@@ -1,11 +1,11 @@
-(ns hello-world.core
-  (:gen-class)
-  (:use ring.adapter.jetty))
+(ns helloworld.core
+  (:use ring.adapter.jetty)
+  (:gen-class))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World"})
 
-(defn -main []
+(defn -main [& args]
   (run-jetty handler {:port 8080}))
