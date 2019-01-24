5b5341f6f5aa047214ab45901272f803240f26af
Steren <steren.giannini@gmail.com>
2019-01-23 16:18:34 -0800

    Use template literals in node sample (#801)
    * Use template literals in node sample
    
    * update README

--- serving/samples/helloworld-nodejs/app.js
+++ serving/samples/helloworld-nodejs/app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send('Hello ' + target + '!');
+  res.send(`Hello ${target}!`);
 });
 
 const port = process.env.PORT || 8080;
