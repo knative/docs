4ccea6cb9307f789df52706bc3c373382802d128
Mete Atamel <atamel@google.com>
2019-02-28 07:22:23 +0000

    Updated instructions to use latest SDK, cleaned up unnecessary imports and added a verification step

--- serving/samples/helloworld-csharp/Startup.cs
+++ serving/samples/helloworld-csharp/Startup.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
