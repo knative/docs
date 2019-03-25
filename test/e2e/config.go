789ca781fdbc6e69f664f79ccec14f7cdc96e029
chaodaiG <45011425+chaodaiG@users.noreply.github.com>
2019-03-25 13:50:50 -0700

    Add generic e2e test base for sample apps (#1057)
    * table driven test
    
    * fix build error
    
    * add e2e build tag to these files to make them not fail unit test
    
    * update based on PR comment
    
    * switch to yaml based config
    
    * make configFile a const
    
    * Temporarily start knative serving for testing purpose
    
    * This reverts commit 75b8fc434b07b5e09759710a130a9dec7c18defc.

---
+++ test/e2e/config.go
@@ -0,0 +1,89 @@
+// +build e2e
+
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2etest
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	// using these defaults if not provided, see useDefaultIfNotProvided function below
+	defaultSrcDir               = "../../docs/serving/samples/hello-world/helloworld-%s"
+	defaultWorkDir              = "helloworld-%s_tmp"
+	defaultAppName              = "helloworld-%s"
+	defaultYamlImagePlaceHolder = "docker.io/{username}/helloworld-%s"
+)
+
+type allConfigs struct {
+	Languages []languageConfig `yaml:"languages`
+}
+
+type languageConfig struct {
+	Language             string    `yaml:"language"`
+	ExpectedOutput       string    `yaml:"expectedOutput"`
+	SrcDir               string    `yaml:"srcDir"`  // Directory contains sample code
+	WorkDir              string    `yaml:"workDir"` // Temp work directory
+	AppName              string    `yaml:"appName"`
+	YamlImagePlaceholder string    `yaml:"yamlImagePlaceholder"` // Token to be replaced by real docker image URI
+	PreCommands          []command `yaml:"preCommands"`          // Commands to be ran before copying
+	Copies               []string  `yaml:"copies"`               // Files to be copied from SrcDir to WorkDir
+	PostCommands         []command `yaml:"postCommands"`         // Commands to be ran after copying
+}
+
+type command struct {
+	Exec string `yaml:"exec"`
+	Args string `yaml:"args"`
+}
+
+func (lc *languageConfig) useDefaultIfNotProvided() {
+	if "" == lc.SrcDir {
+		lc.SrcDir = fmt.Sprintf(defaultSrcDir, lc.Language)
+	}
+	if "" == lc.WorkDir {
+		lc.WorkDir = fmt.Sprintf(defaultWorkDir, lc.Language)
+	}
+	if "" == lc.AppName {
+		lc.AppName = fmt.Sprintf(defaultAppName, lc.Language)
+	}
+	if "" == lc.YamlImagePlaceholder {
+		lc.YamlImagePlaceholder = fmt.Sprintf(defaultYamlImagePlaceHolder, lc.Language)
+	}
+}
+
+func (c *command) run(t *testing.T) {
+	args := strings.Split(c.Args, " ")
+	if output, err := exec.Command(c.Exec, args...).CombinedOutput(); err != nil {
+		t.Fatalf("Error executing: '%s' '%s' -err: '%v'", c.Exec, c.Args, strings.TrimSpace(string(output)))
+	}
+}
+
+func getConfigs(configPath string) (allConfigs, error) {
+	var lcs allConfigs
+	content, err := ioutil.ReadFile(configPath)
+	if nil == err {
+		err = yaml.Unmarshal(content, &lcs)
+	}
+	return lcs, err
+}
---
+++ test/e2e/sampleapp_test.go
@@ -0,0 +1,42 @@
+// +build e2e
+
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2etest
+
+import (
+	"testing"
+)
+
+const (
+	configFile = "config.yaml"
+)
+
+// TestSampleApp runs all sample apps from different languages
+func TestSampleApp(t *testing.T) {
+	lcs, err := getConfigs(configFile)
+	if nil != err {
+		t.Fatalf("Failed reading config file %s: '%v'", configFile, err)
+	}
+
+	for _, lc := range lcs.Languages {
+		lc.useDefaultIfNotProvided()
+		t.Run(lc.Language, func(t *testing.T) {
+			SampleAppTestBase(t, lc, lc.ExpectedOutput)
+		})
+	}
+}
