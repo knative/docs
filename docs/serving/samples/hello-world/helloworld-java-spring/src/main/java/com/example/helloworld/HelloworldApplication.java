30280191ef9f295e55ac675a0ce50d81c48470f9
Averi Kitsch <akitsch@google.com>
2020-02-11 12:10:22 -0800

    Update Java Spring boot sample to programmatically get PORT (#2173)
    * Update Java Springboot sample to programmatically get PORT
    
    * update readme
    
    * Clean up

--- docs/serving/samples/hello-world/helloworld-java-spring/src/main/java/com/example/helloworld/HelloworldApplication.java
+++ docs/serving/samples/hello-world/helloworld-java-spring/src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class HelloworldApplication {
 
-	@Value("${TARGET:World}")
-	String target;
+  @Value("${TARGET:World}")
+  String target;
 
-	@RestController
-	class HelloworldController {
-		@GetMapping("/")
-		String hello() {
-			return "Hello " + target + "!";
-		}
-	}
+  @RestController
+  class HelloworldController {
+    @GetMapping("/")
+    String hello() {
+      return "Hello " + target + "!";
+    }
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloworldApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(HelloworldApplication.class, args);
+  }
 }
