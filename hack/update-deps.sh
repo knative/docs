b6d82f6632dd4cf0ebd355fc7c15c8e6071f11bd
Matt Moore <mattmoor@vmware.com>
2020-03-02 18:14:37 -0800

    Add `--upgrade` to `./hack/update-deps.sh`

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -22,8 +22,26 @@ source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/library.sh
 
 cd ${REPO_ROOT_DIR}
 
+# The list of dependencies that we track at HEAD and periodically
+# float forward in this repository.
+FLOATING_DEPS=(
+  "knative.dev/test-infra"
+)
+
+# Parse flags to determine any we should pass to dep.
+DEP_FLAGS=()
+while [[ $# -ne 0 ]]; do
+  parameter=$1
+  case ${parameter} in
+    --upgrade) DEP_FLAGS=( -update ${FLOATING_DEPS[@]} ) ;;
+    *) abort "unknown option ${parameter}" ;;
+  esac
+  shift
+done
+readonly DEP_FLAGS
+
 # Ensure we have everything we need under vendor/
-dep ensure
+dep ensure ${DEP_FLAGS[@]}
 
 # Remove the cmd dir from eventing.
 rm -rf vendor/github.com/knative/eventing/cmd/
