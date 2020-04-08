6f67e171ae2a0663b601c301969f7f10de17dd2f
Matt Moore <mattmoor@vmware.com>
2020-04-08 06:35:01 -0700

    [release-0.13] Auto-update dependencies (#2362)
    Produced via:
      `./hack/update-deps.sh --upgrade && ./hack/update-codegen.sh`
    /assign dprotaso grantr samodell
    /cc dprotaso grantr samodell

---
+++ vendor/knative.dev/pkg/test/helpers/name.go
@@ -0,0 +1,99 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package helpers
+
+import (
+	"log"
+	"math/rand"
+	"strings"
+	"time"
+	"unicode"
+
+	"knative.dev/pkg/test"
+)
+
+const (
+	letterBytes    = "abcdefghijklmnopqrstuvwxyz"
+	randSuffixLen  = 8
+	sep            = '-'
+	testNamePrefix = "Test"
+)
+
+func init() {
+	// Properly seed the random number generator so AppendRandomString() is actually random.
+	// Otherwise, rerunning tests will generate the same names for the test resources, causing conflicts with
+	// already existing resources.
+	seed := time.Now().UTC().UnixNano()
+	log.Printf("Using '%d' to seed the random number generator", seed)
+	rand.Seed(seed)
+}
+
+// ObjectPrefixForTest returns the name prefix for this test's random names.
+func ObjectPrefixForTest(t test.T) string {
+	return MakeK8sNamePrefix(strings.TrimPrefix(t.Name(), testNamePrefix))
+}
+
+// ObjectNameForTest generates a random object name based on the test name.
+func ObjectNameForTest(t test.T) string {
+	return AppendRandomString(ObjectPrefixForTest(t))
+}
+
+// AppendRandomString will generate a random string that begins with prefix.
+// This is useful if you want to make sure that your tests can run at the same
+// time against the same environment without conflicting.
+// This method will use "-" as the separator between the prefix and
+// the random suffix.
+func AppendRandomString(prefix string) string {
+	suffix := make([]byte, randSuffixLen)
+
+	for i := range suffix {
+		suffix[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+
+	return strings.Join([]string{prefix, string(suffix)}, string(sep))
+}
+
+// MakeK8sNamePrefix converts each chunk of non-alphanumeric character into a single dash
+// and also convert camelcase tokens into dash-delimited lowercase tokens.
+func MakeK8sNamePrefix(s string) string {
+	var sb strings.Builder
+	newToken := false
+	for _, c := range s {
+		if !(unicode.IsLetter(c) || unicode.IsNumber(c)) {
+			newToken = true
+			continue
+		}
+		if sb.Len() > 0 && (newToken || unicode.IsUpper(c)) {
+			sb.WriteRune(sep)
+		}
+		sb.WriteRune(unicode.ToLower(c))
+		newToken = false
+	}
+	return sb.String()
+}
+
+// GetBaseFuncName returns the baseFuncName parsed from the fullFuncName.
+// eg. test/e2e.TestMain will return TestMain.
+func GetBaseFuncName(fullFuncName string) string {
+	name := fullFuncName
+	// Possibly there is no parent package, so only remove it from the name if '/' exists
+	if strings.ContainsRune(name, '/') {
+		name = name[strings.LastIndex(name, "/")+1:]
+	}
+	name = name[strings.LastIndex(name, ".")+1:]
+	return name
+}
