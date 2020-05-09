30b0fdaca9bd07c22cf1e255f46955f997e49eaa
Mustafa Demirhan <mdemirhan@google.com>
2020-05-08 18:43:15 -0700

    Fix the source url to be consistent with the sample name.

--- docs/serving/samples/cloudevents/cloudevents-dotnet/Constants.cs
+++ docs/serving/samples/cloudevents/cloudevents-dotnet/Constants.cs
@@ -5,6 +5,6 @@ namespace CloudEventsSample
         public const string ResponseType = "dev.knative.docs.sample";
 
         public const string ResponseUri =
-            "https://github.com/knative/docs/docs/serving/samples/cloudevents/cloudevents-csharp";
+            "https://github.com/knative/docs/docs/serving/samples/cloudevents/cloudevents-dotnet";
     }
 }
