ba85a262120f6ccd40c08530a78ed8d066827e49
Grant Timmerman <timmerman+devrel@google.com>
2020-05-12 14:29:39 -0700

    fix: use env var for PORT if not specified
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- community/samples/serving/helloworld-deno/main.ts
+++ community/samples/serving/helloworld-deno/main.ts
@@ -1,6 +1,7 @@
 import { serve } from "./deps.ts";
+import "https://deno.land/x/dotenv/load.ts";
 
-const PORT = 8080;
+const PORT = Deno.env('PORT') || 8080;
 const s = serve(`0.0.0.0:${PORT}`);
 const body = new TextEncoder().encode("Hello Deno\n");
 
