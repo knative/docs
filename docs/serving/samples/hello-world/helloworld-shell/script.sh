591757ccf11e1503a3ccb4aa27766280b62b593f
Roland Huß <roland@ro14nd.de>
2020-11-19 16:01:51 +0100

    sample: Convert to BusyBox httpd for the shell sample (#3049)
    * sample: Convert to BusyBox httpd for the shell sample
    
    nc has issues because of the constant listen and unlisten on port 8080.
    Therefor switching over to busybox's [httpd](https://git.busybox.net/busybox/tree/networking/httpd.c)
    which is should be much more robust.
    
    The shell example still stays simple and reuses an existing command.
    
    * simplified script a bit

--- docs/serving/samples/hello-world/helloworld-shell/script.sh
+++ docs/serving/samples/hello-world/helloworld-shell/script.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-while true ; do
-  echo -e "HTTP/1.1 200\n\n Hello ${TARGET:=World}!\n" | nc -l -p 8080 -q 1;
-done
+
+# Set the configuration to serve the index file plain/text
+echo "I:index.txt" > httpd.conf
+
+# Prepare this index file with a nice welcome message
+# Use environment variable TARGET or "World" if not set
+echo "Hello ${TARGET:=World}!" > index.txt
+
+# Start up busybox's httpd service, listen on port 8080 and
+# stay in the foreground. Prints out verbose logs, too.
+# See https://git.busybox.net/busybox/tree/networking/httpd.c for
+# details
+httpd -vv -p 8080 -f
