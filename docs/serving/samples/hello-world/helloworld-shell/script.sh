138ebcf2120fb8cbad6b100e7dc6e01acdf860f5
Roland Huß <roland@ro14nd.de>
2020-11-19 12:32:26 +0100

    simplified script a bit

--- docs/serving/samples/hello-world/helloworld-shell/script.sh
+++ docs/serving/samples/hello-world/helloworld-shell/script.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# Welcome message to show, evaluation TARGET env but fallback to "World"
-message="Hello ${TARGET:=World}!"
-
-# Set the configuration to server the index file
+# Set the configuration to serve the index file plain/text
 echo "I:index.txt" > httpd.conf
 
-# Prepare index file that should be served
-echo $message > index.txt
+# Prepare this index file with a nice welcome message
+# Use environment variable TARGET or "World" if not set
+echo "Hello ${TARGET:=World}!" > index.txt
 
 # Start up busybox's httpd service, listen on port 8080 and
 # stay in the foreground. Prints out verbose logs, too.
