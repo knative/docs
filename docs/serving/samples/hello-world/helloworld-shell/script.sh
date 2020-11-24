d8350c193af72a95797e852b853811147d02cbce
Roland Huß <roland@ro14nd.de>
2020-11-24 04:06:34 +0100

    helloworld shell sample: Switch to use CGI bin for the output (#3053)
    * helloworld shell sample: Switch to use CGI bin for the output
    
    Also, httpd is now started by init for proper SIGTERM handling.
    If you run the image itself within Docker, note that SIGINT (CTRL-C) is not captured.
    Use `docker --init` for being able to stop the containe with CTRL-C
    (This is not relevant when used for Knative)
    
    Thanks to @csantanapr for the idea to switch to CGI.
    
    * chore: Removed dynamic parts from the script
    
    It's back now to a simple echo, so that the CI tests pass that
    compare on exactly the output of a request. Also this is probably
    easier to understand for a user, too.
    
    Based on review feedback.
    
    * chore: Add missing file

--- docs/serving/samples/hello-world/helloworld-shell/script.sh
+++ docs/serving/samples/hello-world/helloworld-shell/script.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# Set the configuration to serve the index file plain/text
-echo "I:index.txt" > httpd.conf
+# Print out CGI header
+# See https://tools.ietf.org/html/draft-robinson-www-interface-00
+# for the full CGI specification
+echo -e "Content-Type: text/plain\n"
 
-# Prepare this index file with a nice welcome message
 # Use environment variable TARGET or "World" if not set
-echo "Hello ${TARGET:=World}!" > index.txt
+echo "Hello ${TARGET:=World}!"
 
-# Start up busybox's httpd service, listen on port 8080 and
-# stay in the foreground. Prints out verbose logs, too.
-# See https://git.busybox.net/busybox/tree/networking/httpd.c for
-# details
-httpd -vv -p 8080 -f
+# In this script you can perform more dynamic actions, too.
+# Like printing the date, checking CGI environment variables, ...
