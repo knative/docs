8a4b4e8988b55be124ed1fbfd2e3c223b296027f
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2021-01-28 10:35:31 +0900

    Fix CI issue by pulling download-istio.sh (#3182)

--- vendor/knative.dev/net-istio/third_party/placeholder.go
+++ vendor/knative.dev/net-istio/third_party/placeholder.go
@@ -1,14 +1,9 @@
-// +build tools
-
 /*
 Copyright 2020 The Knative Authors
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
+    https://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,12 +11,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package tools
-
-import (
-	_ "knative.dev/hack"
-
-	// For istio installation
-	_ "knative.dev/net-istio/third_party"
-	_ "knative.dev/net-istio/third_party/istio-stable"
-)
+// Package config exists to make the config directory importable.
+package config
