6400963beb617d03de821a71b1ffdd239ff2f3bc
Julz Friedman <julz.friedman@uk.ibm.com>
2021-06-22 16:34:18 +0100

    Fix integration tests

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -16,10 +16,40 @@
 
 source $(dirname $0)/../vendor/knative.dev/hack/e2e-tests.sh
 
+# Copied from knative/serving setup script:
+# https://github.com/knative/serving/blob/main/test/e2e-networking-library.sh#L17
 function install_istio() {
-  echo ">> Bringing up Istio"
-  curl -sL https://istio.io/downloadIstioctl | sh -
-  $HOME/.istioctl/bin/istioctl install -y
+  if [[ -z "${ISTIO_VERSION:-}" ]]; then
+    readonly ISTIO_VERSION="stable"
+  fi
+  header "Installing Istio ${ISTIO_VERSION}"
+
+  LATEST_NET_ISTIO_RELEASE_VERSION=$(
+  curl -L --silent "https://api.github.com/repos/knative/net-istio/releases" | grep '"tag_name"' \
+    | cut -f2 -d: | sed "s/[^v0-9.]//g" | sort | tail -n1)
+
+  # And checkout the setup script based on that release
+  local NET_ISTIO_DIR=$(mktemp -d)
+  (
+    cd $NET_ISTIO_DIR \
+      && git init \
+      && git remote add origin https://github.com/knative-sandbox/net-istio.git \
+      && git fetch --depth 1 origin $LATEST_NET_ISTIO_RELEASE_VERSION \
+      && git checkout FETCH_HEAD
+  )
+
+  if [[ -z "${ISTIO_PROFILE:-}" ]]; then
+    readonly ISTIO_PROFILE="istio-ci-no-mesh.yaml"
+  fi
+
+  if [[ -n "${CLUSTER_DOMAIN:-}" ]]; then
+    sed -ie "s/cluster\.local/${CLUSTER_DOMAIN}/g" ${NET_ISTIO_DIR}/third_party/istio-${ISTIO_VERSION}/${ISTIO_PROFILE}
+  fi
+
+  echo ">> Installing Istio"
+  echo "Istio version: ${ISTIO_VERSION}"
+  echo "Istio profile: ${ISTIO_PROFILE}"
+  ${NET_ISTIO_DIR}/third_party/istio-${ISTIO_VERSION}/install-istio.sh ${ISTIO_PROFILE}
 }
 
 function test_setup() {
