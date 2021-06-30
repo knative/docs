f2c6b6cf8eb75c87766de0a1a17aedda653e014a
Julz Friedman <julz.friedman@uk.ibm.com>
2021-06-30 17:02:14 +0100

    Mv old-docs archived

--- hack/build.sh
+++ hack/build.sh
@@ -137,10 +137,10 @@ fi
 
 # Temporarily, copy staticly built old versions of non-mkdocs docs until these scroll out of support
 # TODO(jz) remove these each release until they disappear!
-cp -r old-docs/scss/* site/scss/
-cp -r old-docs/v0.23-docs site/v0.23-docs
-cp -r old-docs/v0.22-docs site/v0.22-docs
-cp -r old-docs/v0.21-docs site/v0.21-docs
+cp -r archived/scss/* site/scss/
+cp -r archived/v0.23-docs site/v0.23-docs
+cp -r archived/v0.22-docs site/v0.22-docs
+cp -r archived/v0.21-docs site/v0.21-docs
 
 # Home page is served from docs, so add a redirect.
 # TODO(jz) in production this should be done with a netlify 301 (or maybe just copy docs/index up with a base set).
