a24b819f1c81a17b918aa09637e78bc7c8037820
Julian Friedman <julz.friedman@uk.ibm.com>
2021-07-07 13:54:18 +0100

    Add feature stable label macro and document that DomainMapping is beta (#3913)
    * Add feature stable label macro
    
    * Put description in expandable section, tweak padding

--- hack/macros.py
+++ hack/macros.py
@@ -3,6 +3,21 @@ import os
 def define_env(env):
 
     @env.macro
+    def feature(alpha="", beta="", stable=""):
+        versions = []
+        descriptions = []
+        if alpha != "":
+            versions.append('<span class="feature-alpha">alpha</span> since Knative v{version}'.format(version=alpha))
+            descriptions.append('    - <span class="feature-alpha">alpha</span> features are experimental, and may change or be removed without notice.')
+        if beta != "":
+            versions.append('<span class="feature-beta">beta</span> since Knative v{version}'.format(version=beta))
+            descriptions.append('    - <span class="feature-beta">beta</span> features are well-tested and enabling them is considered safe. Support for the overall feature will not be dropped, though details may change in incompatible ways.')
+        if stable != "":
+            versions.append('<span class="feature-stable">stable</span> since Knative v{version}'.format(version=stable))
+            descriptions.append('    - <span class="feature-stable">stable</span> features will be maintained for many future versions.')
+        return '??? info "Feature Availability: ' + ', '.join(versions) + '"\n' + '\n'.join(descriptions)
+
+    @env.macro
     def artifact(repo, file, org="knative"):
         """Generates a download link for the current release version.
 
