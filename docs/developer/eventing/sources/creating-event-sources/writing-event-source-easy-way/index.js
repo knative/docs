648cd6b283cc69d34b4680c85f23d2610c6d1c59
Lance Ball <lball@redhat.com>
2021-08-31 15:46:32 -0400

    eventing(samples): update Node.js CloudEvents ContainerSource example (#4039)
    * eventing(samles): update Node.js CloudEvents ContainerSource
    
    The example had some old versions of Node and CloudEvents. This change
    updates the versions for those dependencies, and makes some modifications
    to the example code based on these dependency changes.
    
    Signed-off-by: Lance Ball <lball@redhat.com>
    
    * fixup: review feedback
    
    Co-authored-by: Samia Nneji <snneji@vmware.com>
    
    Co-authored-by: Samia Nneji <snneji@vmware.com>

--- docs/developer/eventing/sources/creating-event-sources/writing-event-source-easy-way/index.js
+++ docs/developer/eventing/sources/creating-event-sources/writing-event-source-easy-way/index.js
@@ -1,54 +1,54 @@
-const { CloudEvent, HTTPEmitter } = require("cloudevents-sdk");
+const got = require('got');
+const { CloudEvent, Emitter, emitterFor } = require('cloudevents');
 
-let sinkUrl = process.env['K_SINK'];
+const K_SINK = process.env['K_SINK'];
+K_SINK || logExit('Error: K_SINK Environment variable is not defined');
+console.log(`Sink URL is ${K_SINK}`);
 
-console.log("Sink URL is " + sinkUrl);
-
-let emitter = new HTTPEmitter({
-    url: sinkUrl
-});
+const source = 'urn:event:from:heartbeat/example';
+const type = 'heartbeat.example';
 
 let eventIndex = 0;
-setInterval(function () {
-    console.log("Emitting event #" + ++eventIndex);
+setInterval(() => {
+  console.log(`Emitting event # ${++eventIndex}`);
 
-    let myevent = new CloudEvent({
-        source: "urn:event:from:my-api/resource/123",
-        type: "your.event.source.type",
-        id: "your-event-id",
-        dataContentType: "application/json",
-        data: {"hello": "World " + eventIndex},
-    });
+  // Create a new CloudEvent each second
+  const event = new CloudEvent({ source, type, data: {'hello': `World # ${eventIndex}`} });
 
-    // Emit the event
-    emitter.send(myevent)
-        .then(response => {
-            // Treat the response
-            console.log("Event posted successfully");
-            console.log(response.data);
-        })
-        .catch(err => {
-            // Deal with errors
-            console.log("Error during event post");
-            console.error(err);
-        });
+  // Emits the 'cloudevent' Node.js event application-wide
+  event.emit();
 }, 1000);
+
+// Create a function that can post an event
+const emit = emitterFor(event => {
+  got.post(K_SINK, event)
+    .then(response => {
+      console.log('Event posted successfully');
+      console.log(response.data);
+    })
+    .catch(err => {
+      console.log('Error during event post');
+      console.error(err);
+    });
+  });
+
+// Send the CloudEvent any time a Node.js 'cloudevent' event is emitted
+Emitter.on('cloudevent', emit);
 
 registerGracefulExit();
 
 function registerGracefulExit() {
-    let logExit = function () {
-        console.log("Exiting");
-        process.exit();
-    };
+  process.on('exit', logExit);
+  //catches ctrl+c event
+  process.on('SIGINT', logExit);
+  process.on('SIGTERM', logExit);
+  // catches 'kill pid' (for example: nodemon restart)
+  process.on('SIGUSR1', logExit);
+  process.on('SIGUSR2', logExit);
+}
 
-    // handle graceful exit
-    //do something when app is closing
-    process.on('exit', logExit);
-    //catches ctrl+c event
-    process.on('SIGINT', logExit);
-    process.on('SIGTERM', logExit);
-    // catches "kill pid" (for example: nodemon restart)
-    process.on('SIGUSR1', logExit);
-    process.on('SIGUSR2', logExit);
+function logExit(message = 'Exiting...') {
+  // Handle graceful exit
+  console.log(message);
+  process.exit();
 }
