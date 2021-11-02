050c595185dfac4fbdd6bb34473ac6517e52bd9c
Carlos Santana <csantana23@gmail.com>
2021-11-02 17:54:40 -0400

    cut docs for Knative 1.0 GA (#4418)
    * starting to cut 1.0 docs for knative
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * adjust for warning for old version to not have prefix knative-
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * using upstreeam repo
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * remove temp knative from repo name
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hack/build.sh
+++ hack/build.sh
@@ -19,11 +19,11 @@ set -x
 # 1) Make a release-NN branch as normal.
 # 2) Update VERSIONS below (on main) to include the new version.
 #    Order matters :-), Most recent first.
-VERSIONS=("0.26" "0.25" "0.24" "0.23")                  # Docs version, results in the url e.g. knative.dev/docs-0.23/..
-VERSIONS_GENERATORS=("mkdocs" "mkdocs" "mkdocs" "hugo")  # update this to always be 4 in the next two releases replace hugo with mkdocs, remove the copy of static hugo site at the bottom
-RELEASE_BRANCHES=("v0.26.0" "v0.25.0" "v0.24.0")                     # Release version for serving/eventing yaml files and api references.
+VERSIONS=("1.0" "0.26" "0.25" "0.24")                  # Docs version, results in the url e.g. knative.dev/docs-0.23/..
+VERSIONS_GENERATORS=("mkdocs" "mkdocs" "mkdocs" "mkdocs")  # update this to always be 4 in the next two releases replace hugo with mkdocs, remove the copy of static hugo site at the bottom
+RELEASE_BRANCHES=("knative-v1.0.0" "v0.26.0" "v0.25.0" "v0.24.0")                     # Release version for serving/eventing yaml files and api references.
 # 3) For now, set branches and repos for old versions of docs. (This will go away when all docs branches are release-$version).
-DOCS_BRANCHES=("release-0.26" "release-0.25" "release-0.24") # add a branch here for the next 2 releases until everything is mkdocs
+DOCS_BRANCHES=("release-1.0" "release-0.26" "release-0.25" "release-0.24") # add a branch here for the next 2 releases until everything is mkdocs
 REPOS=("knative" "knative" "knative" "knative")
 # 4) PR the result to main.
 # 5) Party.
@@ -66,7 +66,7 @@ else
       git clone --depth 1 -b ${DOCS_BRANCHES[$i+1]} https://github.com/${REPOS[i+1]}/docs "$TEMP/docs-$version"
       curl -f -L --show-error https://raw.githubusercontent.com/knative/serving/${RELEASE_BRANCHES[i+1]}/docs/serving-api.md -s > "$TEMP/docs-$version/docs/reference/api/serving-api.md"
       curl -f -L --show-error https://raw.githubusercontent.com/knative/eventing/${RELEASE_BRANCHES[i+1]}/docs/eventing-api.md -s > "$TEMP/docs-$version/docs/reference/api/eventing-api.md"
-      pushd "$TEMP/docs-$version"; KNATIVE_VERSION=${RELEASE_BRANCHES[i+1]} SAMPLES_BRANCH="${DOCS_BRANCHES[i+1]}" VERSION_WARNING=true mkdocs build -d "$SITE/v$version-docs"; popd
+      pushd "$TEMP/docs-$version"; KNATIVE_VERSION=${RELEASE_BRANCHES[i+1]//knative-} SAMPLES_BRANCH="${DOCS_BRANCHES[i+1]}" VERSION_WARNING=true mkdocs build -d "$SITE/v$version-docs"; popd
     fi
 
   done
@@ -147,8 +147,8 @@ fi
 
 # Temporarily, copy staticly built old versions of non-mkdocs docs until these scroll out of support
 # TODO(jz) remove these each release until they disappear!
-cp -r archived/scss/* site/scss/
-cp -r archived/v0.23-docs site/v0.23-docs
+#cp -r archived/scss/* site/scss/
+#cp -r archived/v0.23-docs site/v0.23-docs
 
 # Home page is served from docs, so add a redirect.
 # TODO(jz) in production this should be done with a netlify 301 (or maybe just copy docs/index up with a base set).
