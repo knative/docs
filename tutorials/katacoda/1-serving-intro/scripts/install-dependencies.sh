6f22bd56b156cf9adb27c0e4d72b1cc43bdbd875
Juan Sanin <jsanin@vmware.com>
2022-01-14 10:58:32 -0500

    Modify katacoda, tried to use quickstart with minikube
    - kn 1.1 download and copied PATH
    - kn-quickstart and copied to PATH
    - install quickstart for minikube
    
    Change base image to ubuntu:2004 and install minikube in the install-dependencies.sh script
    
    Issues running minikube with root. Try to fix that

--- tutorials/katacoda/1-serving-intro/scripts/install-dependencies.sh
+++ tutorials/katacoda/1-serving-intro/scripts/install-dependencies.sh
@@ -1,12 +1,30 @@
+echo "Installing minikube latest..."
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+echo "Done"
+
 echo "Installing kn cli..."
-wget https://storage.googleapis.com/knative-nightly/client/latest/kn-linux-amd64 -O kn
+wget https://github.com/knative/client/releases/download/knative-v1.1.0/kn-linux-amd64 -O kn
 chmod +x kn
 mv kn /usr/local/bin/
 echo "Done"
 
 echo "Waiting for Kubernetes to start. This may take a few moments, please wait..."
+useradd -u 1024 -g docker -m docker
+sudo -H -u docker bash -c 'minikube start'
+minikube start
 while [ `minikube status &>/dev/null; echo $?` -ne 0 ]; do sleep 1; done
 echo "Kubernetes Started"
 
+echo "Installing quickstart ..."
+wget https://github.com/knative-sandbox/kn-plugin-quickstart/releases/download/knative-v1.1.0/kn-quickstart-linux-amd64 -O kn-quickstart
+chmod +x kn-quickstart
+mv kn-quickstart /usr/local/bin/
+echo "Done"
+
 export latest_version=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/knative/serving/tags?per_page=1 | jq -r .[0].name)
 echo "Latest knative version is: ${latest_version}"
+
+echo "Installing quickstart ..."
+kn quickstart minikube
+echo "Done"
