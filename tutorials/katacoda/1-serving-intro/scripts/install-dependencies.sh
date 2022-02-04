8dc8bb21b39600be0ad9966d3be572661c1a9959
Juan Sanin <jsanin@vmware.com>
2022-02-04 15:41:31 -0500

    Minikube had memory requirements that the based image did not meet. Trying out kind

--- tutorials/katacoda/1-serving-intro/scripts/install-dependencies.sh
+++ tutorials/katacoda/1-serving-intro/scripts/install-dependencies.sh
@@ -1,7 +1,15 @@
-echo "Installing minikube latest..."
-wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
-echo "Done"
+#echo "Installing minikube latest..."
+#wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+#sudo install minikube-linux-amd64 /usr/local/bin/minikube
+#echo "Done"
+
+
+echo "Waiting for Kubernetes to start. This may take a few moments, please wait..."
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
+#kind create cluster --wait 30s
+echo "Kubernetes Started"
 
 echo "Installing kn cli..."
 wget https://github.com/knative/client/releases/download/knative-v1.1.0/kn-linux-amd64 -O kn
@@ -9,12 +17,11 @@ chmod +x kn
 mv kn /usr/local/bin/
 echo "Done"
 
-echo "Waiting for Kubernetes to start. This may take a few moments, please wait..."
-useradd -u 1024 -g docker -m docker
-sudo -H -u docker bash -c 'minikube start'
-minikube start
-while [ `minikube status &>/dev/null; echo $?` -ne 0 ]; do sleep 1; done
-echo "Kubernetes Started"
+#echo "Waiting for Kubernetes to start. This may take a few moments, please wait..."
+#useradd -u 1024 -g docker -m docker
+#sudo -H -u docker bash -c 'minikube start'
+#while [ `minikube status &>/dev/null; echo $?` -ne 0 ]; do sleep 1; done
+#echo "Kubernetes Started"
 
 echo "Installing quickstart ..."
 wget https://github.com/knative-sandbox/kn-plugin-quickstart/releases/download/knative-v1.1.0/kn-quickstart-linux-amd64 -O kn-quickstart
@@ -26,5 +33,5 @@ export latest_version=$(curl -H "Accept: application/vnd.github.v3+json" https:/
 echo "Latest knative version is: ${latest_version}"
 
 echo "Installing quickstart ..."
-kn quickstart minikube
+kn quickstart kind
 echo "Done"
