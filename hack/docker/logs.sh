9aa7be96e6b47a8f9dd6e09ece339a330c6fa1d6
Carlos Santana <csantana23@gmail.com>
2022-02-14 18:39:38 -0500

    Add docker instructions to preview website locally
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hack/docker/logs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -x
-
-NAME=${1:-knative-docs-dev}
-docker logs -f ${NAME}
--- hack/docker/test.sh
+++ hack/docker/test.sh
@@ -4,6 +4,6 @@ set -x
 
 NAME=${1:-knative-docs-dev}
 PORT=${2:-8000}
-IMAGE=${3:-knative-docs-dev}
+IMAGE=${3:-ghcr.io/knative/knative-docs:latest}
 
 docker run --rm -v "${PWD}:/site" -it --entrypoint "" ${IMAGE} hack/build.sh
