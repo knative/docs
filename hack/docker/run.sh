a3e16fd7c63fb5e15ccebac839b1456e35ce14bd
Carlos Santana <csantana23@gmail.com>
2022-02-14 20:35:58 -0500

    Docker instructions (#4733)
    * Add docker instructions to preview website locally
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>
    
    * minor updates
    
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hack/docker/run.sh
+++ hack/docker/run.sh
@@ -4,9 +4,10 @@ set -x
 
 NAME=${1:-knative-docs-dev}
 PORT=${2:-8000}
-IMAGE=${3:-knative-docs-dev}
+IMAGE=${3:-ghcr.io/knative/knative-docs:latest}
+
+docker rm "${NAME}" --force 1> /dev/null 2> /dev/null
 
 docker run --name "${NAME}" -d -p "${PORT}:8000" -v "${PWD}:/site" ${IMAGE} serve --dirtyreload --dev-addr=0.0.0.0:8000
 echo "Dev environment running with live reloading enabled. Open http://localhost:${PORT} to see the site"
-echo "For live logs run:"
-echo "docker logs -f ${NAME}"
+docker logs -f ${NAME}
