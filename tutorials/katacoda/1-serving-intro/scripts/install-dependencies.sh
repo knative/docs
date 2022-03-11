ab5b71bf289e3f3b82e4d6e47fc779cc6d275c4e
Juan Sanin <jsanin@vmware.com>
2022-03-11 15:20:15 -0500

    Correct launch.sh call, install knative compatible with k8s 1.18

--- tutorials/katacoda/1-serving-intro/scripts/install-dependencies.sh
+++ tutorials/katacoda/1-serving-intro/scripts/install-dependencies.sh
@@ -51,4 +51,25 @@
 
 
 
-./launch.sh
+launch.sh
+
+#kubectl apply -f https://github.com/knative/serving/releases/download/v0.26.0/serving-crds.yaml
+#kubectl apply -f https://github.com/knative/serving/releases/download/v0.26.0/serving-core.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.3/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.3/serving-core.yaml
+
+kubectl apply -f https://github.com/knative-sandbox/net-contour/releases/download/v0.22.1/contour.yaml
+kubectl apply -f https://github.com/knative-sandbox/net-contour/releases/download/v0.22.1/net-contour.yaml
+
+kubectl patch configmap/config-network \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"ingress.class":"contour.ingress.networking.knative.dev"}}'
+
+
+echo "Installing quickstart ..."
+wget https://github.com/knative-sandbox/kn-plugin-quickstart/releases/download/knative-v1.2.0/kn-quickstart-linux-amd64 -O kn-quickstart
+chmod +x kn-quickstart
+mv kn-quickstart /usr/local/bin/
+echo "Done"
+
