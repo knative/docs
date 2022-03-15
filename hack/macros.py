9dd3d98da9bcdbde694cdc68382cb403928920ff
Dave Protasowski <dprotaso@gmail.com>
2022-03-15 18:44:40 -0400

    update macro to lookup point releases for artifacts

--- hack/macros.py
+++ hack/macros.py
@@ -1,6 +1,40 @@
 import os
+import semver
+import sys
+import traceback
+
+from github import Github
+
+# By default mkdocs swallows print() messages from macros
+def print_to_stdout(*a):
+    print(*a, file = sys.stdout)
+
+class GithubReleases:
+    def __init__(self):
+        self.tags_for_repo = {}
+        self.g = Github(os.getenv("GITHUB_TOKEN"))
+
+    def latest_release(self, major_minor, org, repo):
+        key = f'{org}/{repo}'
+
+        if key not in self.tags_for_repo:
+            tags = []
+            for release in self.g.get_repo(key, lazy=True).get_releases():
+                tags.append(release.tag_name)
+            tags = map(lambda tag: tag.removeprefix("knative-v"), tags)
+            self.tags_for_repo[key] = list(tags)
+
+        tags = self.tags_for_repo[key].copy()
+        tags = list(filter(lambda tag: tag.startswith(major_minor), tags))
+        tags.sort(key=semver.VersionInfo.parse, reverse=True)
+
+        if len(tags) > 0:
+            return tags[0]  
+        else:
+            return None
 
 def define_env(env):
+    g = GithubReleases()
 
     @env.macro
     def feature(alpha="", beta="", stable=""):
@@ -25,24 +59,32 @@ def define_env(env):
         empty this links to googlestorage, otherwise it links via
         the matching release in github.
         """
+
         version = os.environ.get("KNATIVE_VERSION")
+
         if version == None:
-            return 'https://storage.googleapis.com/knative-nightly/{repo}/latest/{file}'.format(
-                    repo=repo,
-                    file=file)
-        else:
-            if version.startswith("v1."):
-                return 'https://github.com/{org}/{repo}/releases/download/knative-{version}/{file}'.format(
-                    repo=repo,
-                    file=file,
-                    version=version,
-                    org=org)
+            return f'https://storage.googleapis.com/knative-nightly/{repo}/latest/{file}'
+        
+        version = version.removeprefix('v')
+
+        try:            
+            v = semver.VersionInfo.parse(version)
+            latest_version = g.latest_release(f'{v.major}.{v.minor}', org, repo)
+
+            if latest_version is None:
+                print_to_stdout(f'repo "{org}/{repo}" has no tags using latest release for file "{file}"')
+                return f'https://github.com/{org}/{repo}/releases/latest/download/{file}'
+            elif version.startswith("1."):
+                return f'https://github.com/{org}/{repo}/releases/download/knative-v{latest_version}/{file}'
             else:
-                return 'https://github.com/{org}/{repo}/releases/download/{version}/{file}'.format(
-                    repo=repo,
-                    file=file,
-                    version=version,
-                    org=org)
+                return f'https://github.com/{org}/{repo}/releases/download/{latest_version}/{file}'
+        except:
+            # We use sys.exit(1) otherwise the mkdocs build doesn't 
+            # fail on exceptions in macros
+            print_to_stdout(f'exception raised for {org}/{repo}/{file}\n', traceback.format_exc())
+            sys.exit(1)
+
+
 
     @env.macro
     def clientdocs():
