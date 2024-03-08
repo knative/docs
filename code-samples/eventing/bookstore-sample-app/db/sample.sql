7ae6c3f741c71b1faec41fd4c38af19c2460efd6
Scott Hirano <86282459+Zazzscoot@users.noreply.github.com>
2024-03-08 14:13:33 -0500

    Update code-samples/eventing/bookstore-sample-app/db/sample.sql
    Co-authored-by: Calum Murray <cmurray@redhat.com>

--- code-samples/eventing/bookstore-sample-app/db/sample.sql
+++ code-samples/eventing/bookstore-sample-app/db/sample.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS book_reviews(
   CONSTRAINT sentiment_check CHECK (sentiment IN ('positive', 'negative', 'neutral')),
 );
 
-INSERT INTO BookReviews VALUES(1, '2020-01-01 00:00:00', 'This book is great!', 'positive');
-INSERT INTO BookReviews VALUES(2, '2020-01-02 00:02:00', 'This book is terrible!', 'negative');
-INSERT INTO BookReviews VALUES(3, '2020-01-03 00:01:30', 'This book is okay.', 'neutral');
-INSERT INTO BookReviews VALUES(4, '2020-01-04 00:00:00', 'Meh', 'neutral');
+INSERT INTO book_reviews (post_time, content, sentiment) VALUES 
+    ('2020-01-01 00:00:00', 'This book is great!', 'positive'),
+    ('2020-01-02 00:02:00', 'This book is terrible!', 'negative'),
+    ('2020-01-03 00:01:30', 'This book is okay.', 'neutral'),
+    ('2020-01-04 00:00:00', 'Meh', 'neutral');
