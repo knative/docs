6b438465cb0f4eba781b41d9b4d1e938e767708c
Leo Li <leoli@redhat.com>
2024-04-25 16:31:41 -0400

    fix: fix the python format by running black

--- code-samples/eventing/bookstore-sample-app/ML-inappropriate-language-filter/func.py
+++ code-samples/eventing/bookstore-sample-app/ML-inappropriate-language-filter/func.py
@@ -2,24 +2,23 @@ from parliament import Context
 from profanity_check import predict
 from cloudevents.http import CloudEvent
 
+
 # The function to convert the bad word filter result into a CloudEvent
-def create_cloud_event(inputText,data):
+def create_cloud_event(inputText, data):
     attributes = {
-    "type": "knative.sampleapp.badword.filter.response",
-    "source": "bad-word-filter",
-    "datacontenttype": "application/json",
+        "type": "knative.sampleapp.badword.filter.response",
+        "source": "bad-word-filter",
+        "datacontenttype": "application/json",
     }
 
     # Put the bad word filter result into a dictionary
-    data = {
-    "input": inputText,
-    "result": data
-    }
+    data = {"input": inputText, "result": data}
 
     # Create a CloudEvent object
     event = CloudEvent(attributes, data)
 
     return event
+
 
 def inappropriate_language_filter(text):
     profanity_result = predict([text["input"]])
@@ -27,8 +26,9 @@ def inappropriate_language_filter(text):
     if profanity_result[0] == 1:
         result = "bad"
 
-    profanity_event = create_cloud_event(text["input"],result)
+    profanity_event = create_cloud_event(text["input"], result)
     return profanity_event
+
 
 def main(context: Context):
     """
