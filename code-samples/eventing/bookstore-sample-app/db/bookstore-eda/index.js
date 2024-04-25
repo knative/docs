a8a71443915c644d39d8ca81abc6098caa08b275
Leo Li <leoli@redhat.com>
2024-04-25 16:05:45 -0400

    fix: configure node.js so that it won't be restricted by CORS policy

--- code-samples/eventing/bookstore-sample-app/db/bookstore-eda/index.js
+++ code-samples/eventing/bookstore-sample-app/db/bookstore-eda/index.js
@@ -8,7 +8,7 @@ const port = 8000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 
 app.post('/add', async (req, res) => {
     try {
