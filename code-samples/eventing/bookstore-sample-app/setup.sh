35ba8624b3e57b4cccdd29747c470c1e7feef55f
Leo HC Li <36619969+Leo6Leo@users.noreply.github.com>
2024-05-22 01:41:13 -0400

    fix: fix the setup script

--- code-samples/eventing/bookstore-sample-app/setup.sh
+++ code-samples/eventing/bookstore-sample-app/setup.sh
@@ -6,9 +6,9 @@ read -p "Press ENTER to continue or Ctrl+C to abort..."
 
 # Install Knative Serving
 echo "Installing Knative Serving..."
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.13.1/serving-crds.yaml
-kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.13.1/serving-core.yaml
-kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.13.0/kourier.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.14.0/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.14.0/serving-core.yaml
+kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.14.0/kourier.yaml
 
 # Configure Kourier as the default ingress
 kubectl patch configmap/config-network --namespace knative-serving --type merge --patch '{"data":{"ingress-class":"kourier.ingress.networking.knative.dev"}}'
@@ -17,8 +17,8 @@ echo "Knative Serving installed successfully."
 
 # Install Knative Eventing
 echo "Installing Knative Eventing..."
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.13.4/eventing-crds.yaml
-kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.13.4/eventing-core.yaml
+kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.14.0/eventing-crds.yaml
+kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.14.0/eventing-core.yaml
 echo "Knative Eventing installed successfully."
 
 # Install Knative imc broker
@@ -42,9 +42,8 @@ fi
 
 
 
-
 # Prompt for the Docker registry details
-echo "Please provide the details of your Container registry where the sample app images will be pushed."
+echo "Please provide the details of your Container registry to install the Camel-K."
 read -p "Enter the registry hostname (e.g., docker.io or quay.io): " REGISTRY_HOST
 read -p "Enter the registry username: " REGISTRY_USER
 read -s -p "Enter the registry password: " REGISTRY_PASSWORD
@@ -55,6 +54,9 @@ export REGISTRY_HOST=$REGISTRY_HOST
 export REGISTRY_USER=$REGISTRY_USER
 export REGISTRY_PASSWORD=$REGISTRY_PASSWORD
 
+# Set the KO_DOCKER_REPO environment variable
+export KO_DOCKER_REPO=$REGISTRY_HOST/$REGISTRY_USER
+
 # Install the camel-K
 kamel install --registry $REGISTRY_HOST--organization $REGISTRY_USER --registry-auth-username $REGISTRY_USER --registry-auth-password $REGISTRY_PASSWORD
 
@@ -64,21 +66,39 @@ echo "Installing the Sample Bookstore App..., but please follow the instruction 
 read -p "Press ENTER to continue..."
 
 # Install the front end first
+cd frontend
+kubectl apply -f config
 
-TODO: will confirm whether use the pre-built images or build the images locally
+# Install the node-server
+cd ../node-server
+kubectl apply -f config
 
+# Deploy the ML services
+cd ../ML-inappropriate-language-filter
+func deploy -b=s2i -v 
 
-## Prompt for Kafka cluster installation
-#echo "Please install the Kafka cluster using the script provided in knative-extensions/eventing-kafka-broker."
-#read -p "Press ENTER after you have installed the Kafka cluster to continue with the setup of Kafka components..."
+cd ../ML-sentiment-analysis
+func deploy -b=s2i -v
 
-# Install Kafka Channel and Kafka Broker
-#echo "Installing Kafka components..."
-#kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.13.8/eventing-kafka-controller.yaml
-#kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.13.8/eventing-kafka-channel.yaml
-#kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.13.8/eventing-kafka-broker.yaml
+# Install the db
+cd ..
+kubectl apply -f db-service
 
-#echo "Kafka components installation completed successfully."
-echo "The installation of all components is complete. Your cluster is now set up with Knative Serving, Knative Eventing, and Kafka."
+# Install the sequence
+kubectl apply -f sequence/config
 
+# Ask the user to edit the properties file
+echo "Please edit slack-sink/application.properties to provide the webhook URL for Slack."
+read -p "Press ENTER to continue..."
+
+# Create the secret
+kubectl create secret generic slack-credentials --from-file=slack-sink/application.properties
+
+# Install the slack-sink
+kubectl apply -f slack-sink/config
+
+# Ask user to open a new terminal to set the minikube tunnel
+echo "If you are using minikube: Please open a new terminal and run 'sudo minikube tunnel' to expose the services to the outside world."
+read -p "Press ENTER to continue..."
+# FIXME: @pierDipi something wrong here. If run minikube tunnel before bookstore is installed, the localhost:80 will not be accessible
 # End of script
