a6d715035595c6b0f03807764d924d8a8d21334c
Knative Prow Robot <automation+prow-robot@knative.team>
2024-12-29 13:40:12 +0000

    [release-1.15] fix ML example (#6187)
    * fix ML example
    
    * fix dockerize
    
    * type variable df
    
    * fix code
    
    ---------
    
    Co-authored-by: Yasuhiro Matsumoto <mattn.jp@gmail.com>

--- code-samples/community/serving/machinelearning-python-bentoml/iris_classifier.py
+++ code-samples/community/serving/machinelearning-python-bentoml/iris_classifier.py
@@ -1,11 +1,23 @@
-from bentoml import env, artifacts, api, BentoService
-from bentoml.handlers import DataframeHandler
-from bentoml.artifact import SklearnModelArtifact
+import numpy as np
+import bentoml
+from pydantic import Field
+from bentoml.validators import Shape
+from typing_extensions import Annotated
+import joblib
 
-@env(auto_pip_dependencies=True)
-@artifacts([SklearnModelArtifact('model')])
-class IrisClassifier(BentoService):
 
-    @api(DataframeHandler)
-    def predict(self, df):
-        return self.artifacts.model.predict(df)
+@bentoml.service
+class IrisClassifier:
+    iris_model = bentoml.models.get("iris_sklearn:latest")
+
+    def __init__(self):
+        self.model = joblib.load(self.iris_model.path_of("model.pkl"))
+
+    @bentoml.api
+    def predict(
+        self,
+        df: Annotated[np.ndarray, Shape((-1, 4))] = Field(
+            default=[[5.2, 2.3, 5.0, 0.7]]
+        ),
+    ) -> np.ndarray:
+        return self.model.predict(df)
